c7c2e971194fd577a082098a58c0119b764373f5
michalgorowij <michal.gorowij@collibra.com>
2019-01-12 14:43:52 +0100

    Add Job 123

--- jobs/s1_2_extended_single.groovy
+++ jobs/s1_2_extended_single.groovy
@@ -1,19 +1,27 @@
 job('sample-gradle-build') {
     steps {
+        wrappers {
+            timestamps()
+        }
         scm {
             git('https://github.com/michalgorowij/job-dsl-workshop', "master") {
             }
         }
     }
     steps {
-        /*triggers {
+        triggers {
             scm("@midnight")
             scm("0 5 * * 1")
-        }*/
+        }
         gradle {
             tasks("clean")
             tasks("check")
             useWrapper(true)
+        }
+    }
+    publishers {
+        archiveJunit('**/target/surefire-reports/*.xml'){
+            allowEmptyResults()
         }
     }
 }
